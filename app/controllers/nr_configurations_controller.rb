fec787286dfc4ded5b363cf2faac3a652cb38b46
Peter Zingg <peter@zingg.name>
2008-09-06 17:33:09 -0700

    standardizing form_for syntax and resource controller methods

--- app/controllers/nr_configurations_controller.rb
+++ app/controllers/nr_configurations_controller.rb
@@ -1,51 +1,85 @@
 class NrConfigurationsController < ApplicationController
+  # GET /nr_configurations
+  # GET /nr_configurations.xml
   def index
-    list
-    render :action => 'list'
-  end
+    @nr_configurations = NrConfiguration.find(:all)
 
-  # GETs should be safe (see http://www.w3.org/2001/tag/doc/whenToUseGet.html)
-  verify :method => :post, :only => [ :destroy, :create, :update ],
-         :redirect_to => { :action => :list }
-
-  def list
-    @nr_configurations = NrConfiguration.paginate :per_page => 10, :page => (params[:page] || 1)
-  end
-
-  def show
-    @nr_configuration = NrConfiguration.find(params[:id])
-  end
-
-  def new
-    @nr_configuration = NrConfiguration.new
-  end
-
-  def create
-    @nr_configuration = NrConfiguration.new(params[:nr_configuration])
-    if @nr_configuration.save
-      flash[:notice] = 'NetRestore configuration was successfully created.'
-      redirect_to nr_configurations_url
-    else
-      render :action => 'new'
+    respond_to do |format|
+      format.html # index.html.erb
+      format.xml  { render :xml => @nr_configurations }
     end
   end
 
+  # GET /nr_configurations/1
+  # GET /nr_configurations/1.xml
+  def show
+    @nr_configuration = NrConfiguration.find(params[:id])
+
+    respond_to do |format|
+      format.html # show.html.erb
+      format.xml  { render :xml => @nr_configuration }
+    end
+  end
+
+  # GET /nr_configurations/new
+  # GET /nr_configurations/new.xml
+  def new
+    @nr_configuration = NrConfiguration.new
+
+    respond_to do |format|
+      format.html # new.html.erb
+      format.xml  { render :xml => @nr_configuration }
+    end
+  end
+
+  # GET /nr_configurations/1/edit
   def edit
     @nr_configuration = NrConfiguration.find(params[:id])
   end
 
-  def update
-    @nr_configuration = NrConfiguration.find(params[:id])
-    if @nr_configuration.update_attributes(params[:nr_configuration])
-      flash[:notice] = 'NetRestore configuration was successfully updated.'
-      redirect_to nr_configuration_url
-    else
-      render :action => 'edit'
+  # POST /nr_configurations
+  # POST /nr_configurations.xml
+  def create
+    @nr_configuration = NrConfiguration.new(params[:nr_configuration])
+
+    respond_to do |format|
+      if @nr_configuration.save
+        flash[:notice] = 'NetRestore configuration was successfully created.'
+        format.html { redirect_to(@nr_configuration) }
+        format.xml  { render :xml => @nr_configuration, :status => :created, :location => @nr_configuration }
+      else
+        format.html { render :action => "new" }
+        format.xml  { render :xml => @nr_configuration.errors, :status => :unprocessable_entity }
+      end
     end
   end
 
+  # PUT /nr_configurations/1
+  # PUT /nr_configurations/1.xml
+  def update
+    @nr_configuration = NrConfiguration.find(params[:id])
+
+    respond_to do |format|
+      if @nr_configuration.update_attributes(params[:nr_configuration])
+        flash[:notice] = 'NetRestore configuration was successfully updated.'
+        format.html { redirect_to(@nr_configuration) }
+        format.xml  { head :ok }
+      else
+        format.html { render :action => "edit" }
+        format.xml  { render :xml => @nr_configuration.errors, :status => :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /nr_configurations/1
+  # DELETE /nr_configurations/1.xml
   def destroy
-    NrConfiguration.find(params[:id]).destroy
-    redirect_to nr_configurations_url
+    @nr_configuration = NrConfiguration.find(params[:id])
+    @nr_configuration.destroy
+
+    respond_to do |format|
+      format.html { redirect_to(nr_configurations_url) }
+      format.xml  { head :ok }
+    end
   end
 end
--- app/controllers/nr_parameters_controller.rb
+++ app/controllers/nr_parameters_controller.rb
@@ -1,51 +1,85 @@
 class NrParametersController < ApplicationController
+  # GET /nr_parameters
+  # GET /nr_parameters.xml
   def index
-    list
-    render :action => 'list'
-  end
+    @nr_parameters = NrParameter.find(:all)
 
-  # GETs should be safe (see http://www.w3.org/2001/tag/doc/whenToUseGet.html)
-  verify :method => :post, :only => [ :destroy, :create, :update ],
-         :redirect_to => { :action => :list }
-
-  def list
-    @nr_parameters = NrParameter.paginate :per_page => 10, :page => (params[:page] || 1)
-  end
-
-  def show
-    @nr_parameter = NrParameter.find(params[:id])
-  end
-
-  def new
-    @nr_parameter = NrParameter.new
-  end
-
-  def create
-    @nr_parameter = NrParameter.new(params[:nr_parameter])
-    if @nr_parameter.save
-      flash[:notice] = 'NetRestore settings were successfully created.'
-      redirect_to nr_parameters_url
-    else
-      render :action => 'new'
+    respond_to do |format|
+      format.html # index.html.erb
+      format.xml  { render :xml => @nr_parameters }
     end
   end
 
+  # GET /nr_parameters/1
+  # GET /nr_parameters/1.xml
+  def show
+    @nr_parameter = NrParameter.find(params[:id])
+
+    respond_to do |format|
+      format.html # show.html.erb
+      format.xml  { render :xml => @nr_parameter }
+    end
+  end
+
+  # GET /nr_parameters/new
+  # GET /nr_parameters/new.xml
+  def new
+    @nr_parameter = NrParameter.new
+
+    respond_to do |format|
+      format.html # new.html.erb
+      format.xml  { render :xml => @nr_parameter }
+    end
+  end
+
+  # GET /nr_parameters/1/edit
   def edit
     @nr_parameter = NrParameter.find(params[:id])
   end
 
-  def update
-    @nr_parameter = NrParameter.find(params[:id])
-    if @nr_parameter.update_attributes(params[:nr_parameter])
-      flash[:notice] = 'NetRestore settings were successfully updated.'
-      redirect_to nr_parameter_url
-    else
-      render :action => 'edit'
+  # POST /nr_parameters
+  # POST /nr_parameters.xml
+  def create
+    @nr_parameter = NrParameter.new(params[:nr_parameter])
+
+    respond_to do |format|
+      if @nr_parameter.save
+        flash[:notice] = 'NetRestore parameter was successfully created.'
+        format.html { redirect_to(@nr_parameter) }
+        format.xml  { render :xml => @nr_parameter, :status => :created, :location => @nr_parameter }
+      else
+        format.html { render :action => "new" }
+        format.xml  { render :xml => @nr_parameter.errors, :status => :unprocessable_entity }
+      end
     end
   end
 
+  # PUT /nr_parameters/1
+  # PUT /nr_parameters/1.xml
+  def update
+    @nr_parameter = NrParameter.find(params[:id])
+
+    respond_to do |format|
+      if @nr_parameter.update_attributes(params[:nr_parameter])
+        flash[:notice] = 'NetRestore parameter was successfully updated.'
+        format.html { redirect_to(@nr_parameter) }
+        format.xml  { head :ok }
+      else
+        format.html { render :action => "edit" }
+        format.xml  { render :xml => @nr_parameter.errors, :status => :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /nr_parameters/1
+  # DELETE /nr_parameters/1.xml
   def destroy
-    NrParameter.find(params[:id]).destroy
-    redirect_to :action => 'list'
+    @nr_parameter = NrParameter.find(params[:id])
+    @nr_parameter.destroy
+
+    respond_to do |format|
+      format.html { redirect_to(nr_parameters_url) }
+      format.xml  { head :ok }
+    end
   end
 end
